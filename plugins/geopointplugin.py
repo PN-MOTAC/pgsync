84e6e91f4f0dd513bb8716d8b34eeceac21dba44
Ahmad <ahmadbilal.3491@gmail.com>
2025-06-18 17:26:38 +0500

    fix: add dev workflow

--- plugins/geopointplugin.py
+++ plugins/geopointplugin.py
@@ -4,13 +4,25 @@ class GeoPointPlugin(plugin.Plugin):
     name = 'GeoPoint'
 
     def transform(self, doc, **kwargs):
-        # Extract latitude and longitude from the document
-        latitude = doc.get('latitude')
-        longitude = doc.get('longitude')
+        def add_geo_point(obj):
+            if not isinstance(obj, dict):
+                return
 
-        # Add a geo_point field if both lat and lon exist
-        if latitude is not None and longitude is not None:
-            doc['point'] = {'lat': latitude, 'lon': longitude}
+            # Safely check and add 'point' if 'latitude' and 'longitude' are present
+            lat = obj.get('latitude')
+            lon = obj.get('longitude')
+            if lat is not None and lon is not None:
+                obj['point'] = {'lat': lat, 'lon': lon}
 
-        # Return the transformed document
+            # Recurse into nested dicts and lists
+            for key in list(obj.keys()):  # Use list() to avoid dict size change issues
+                value = obj[key]
+                if isinstance(value, dict):
+                    add_geo_point(value)
+                elif isinstance(value, list):
+                    for item in value:
+                        if isinstance(item, dict):
+                            add_geo_point(item)
+
+        add_geo_point(doc)
         return doc
